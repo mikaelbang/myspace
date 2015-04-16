2d49fe14b1ee772d7cdc2fd850bcf1f9405a1805
Mikael Bang <mackeb1@gmail.com>
2015-04-16 15:17:15 +0200

    wall done except for CSS

--- views/js/main.js
+++ views/js/main.js
@@ -20,40 +20,22 @@ $(document).ready(function(){
     });
 
 
-    $(this).find('.comment_form').on('click', function(){
+    $('.commentButton').on('click', function(){
 
-        event.preventDefault();
+        //event.preventDefault();
+
+        /*
 
         var comment = {
             post_id: $(this).find('.hidden_post_id').val()
         };
 
-        $.ajax({
-            type: 'POST',
-            url: '../wall/getComments',
-            dataType: 'json',
-            data: comment,
-            success: function(comments){
+        ajax(comment);
+        */
 
-                for(var i = 0; i < comments.length; i++){
-                    var c = comments[i];
-                    var p_id = c.post_id;
 
-                    $('#others_comments'+ c.post_id +'').prepend('<div class="othersCommentsBorder">' +
-                                                    '<p class="commentBorderText">' + c.first_name + " " + c.last_name + " || " + c.created + '</p>' +
-                                                '</div>' +
-                                                '<div class="othersCommentContent">' +
-                                                    '<p class="othersCommentText">' + c.content + '</p>' +
-                                                '</div>');
-                }
-            },
-            error: function(){
-                alert('Something went wrong');
-            }
-        });
-
-        $(this).parent().parent().siblings().closest('.commentContent').toggle();
-        $(this).parent().parent().siblings('.commentContent').find('.othersComments').empty();
+        $(this).parent().parent().next('.commentContent').toggle();
+        //$(this).parent().parent().siblings('.commentContent').find('.othersComments').empty();
 
 
     });
@@ -99,6 +81,34 @@ $(document).ready(function(){
         $(".postPhoto").css("display","none");
         $(".postSound").css("display","none");
     });
+
+
+    function ajax(comment){
+
+        $.ajax({
+            type: 'POST',
+            url: '../wall/getComments',
+            dataType: 'json',
+            data: comment,
+            success: function(comments){
+
+                for(var i = 0; i < comments.length; i++){
+                    var c = comments[i];
+                    var p_id = c.post_id;
+
+                    $('#others_comments'+ c.post_id +'').prepend('<div class="othersCommentsBorder">' +
+                        '<p class="commentBorderText">' + c.first_name + " " + c.last_name + " || " + c.created + '</p>' +
+                        '</div>' +
+                        '<div class="othersCommentContent">' +
+                        '<p class="othersCommentText">' + c.content + '</p>' +
+                        '</div>');
+                }
+            },
+            error: function(){
+                alert('Something went wrong');
+            }
+        });
+    }
 });
 
 
